dc8449e4cc8e41895ca2639115dab271683de025
lenhatquang1998 <42506288+lenhatquang1998@users.noreply.github.com>
2019-01-03 23:05:52 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="demoweb.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.css"> -->
+    <title>Document</title>
+</head>
+<body id="hinh_nen">
+    <div id="menu">
+        <ul>
+            <li id="a"><a href="#">HOME</a></li>
+            <li id="b">
+                <a href="#">MENU</a>
+                <ul class="sub-menu">
+                    <li><a href="#">Món Ăn Gia Đình</a></li>
+                    <li><a href="#">Món Ăn Cặp Đôi</a></li>
+                    <li><a href="ngay-le.php">Món Ăn Dịp Lễ</a></li>
+                </ul>
+            </li>
+            <li id="c"><a href="#">Log In</a></li>
+        </ul>
+    </div>
+    <div class="image" style="padding-top:60px">
+        <div class="chu_1">VÀO BẾP THÔI NÀO !!!</div>
+        <div class="chu_2">Còn Gì Thú Vị Hơn Khi Thưởng Thức Những Món Ăn Do Chính Tay Ta Nấu</div>
+    <div class="chinh">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" >
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner" style="width: 1000px;height: 500px;">
+              <div class="carousel-item active">
+                <img class="d-block w-100" src="https://chuphinhmonan.com/wp-content/uploads/2017/03/fresh-box.jpg" alt="First slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="http://file.vforum.vn/hinh/2016/08/hinh-anh-dep-ve-thuc-an-2.jpg" alt="Second slide">
+              </div>
+      
+              <div class="carousel-item">
+                <img class="d-block w-100" src="http://farm1.static.flickr.com/406/19307878238_f51e8b5f87_b.jpg" alt="Third slide">
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>   
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    
+    </div>
+        <div class="thu_muc">
+        <a class="gia_dinh" href="">MÓN ĂN GIA ĐÌNH</a>
+        <a class="cap_doi" href="">MÓN ĂN CẶP ĐÔI</a>
+        <a class="dip_le" href="ngay-le.php">MÓN ĂN DỊP LỄ</a>
+    </div>
+    <div class="end">Bài Tập Lớn Môn Công Nghệ Web, K58 Đại Học Thủy Lợi</div>
+
+</body>
+</html>
